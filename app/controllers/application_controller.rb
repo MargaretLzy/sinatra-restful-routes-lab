bc2642fc02712aa8ea14dd5b61d0818c00ae2240
dynamic-daemon-637b <zl3087@columbia.edu>
2021-07-04 01:32:30 +0000

    read

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,21 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
-
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+end
+  get '/recipes/new' do
+    erb:new
+  end
+  post '/recipes' do
+    @recipe = Recipe.create(:name =>params[:name], :ingredient =>params[:ingredient], :cook_time => params[:cook_time])
+    redirect to "/recipes/#{@recipe.id}"
+  end
+  
+  #show
+  get '/recipes/:id' do
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
 end
